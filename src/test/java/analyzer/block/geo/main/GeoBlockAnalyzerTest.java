4aa1e714702c36e70127a867334e4c0ccafd3e6a
Eoin Reilly <eoindreilly@gmail.com>
2020-10-03 19:19:22 +0100

    Minor code improvements

--- src/test/java/analyzer/block/geo/main/GeoBlockAnalyzerTest.java
+++ src/test/java/analyzer/block/geo/main/GeoBlockAnalyzerTest.java
@@ -1,6 +1,7 @@
 package analyzer.block.geo.main;
 
 import analyzer.block.geo.result.GeoResult;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.time.format.DateTimeFormatter;
@@ -10,54 +11,52 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GeoBlockAnalyzerTest {
 
-    private GeoBlockAnalyzer geoBlockAnalyzer;
-    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+  private GeoBlockAnalyzer geoBlockAnalyzer;
+  private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-    @org.junit.jupiter.api.Test
-    void getLargestGeoBlockTestOne() throws IOException {
-        this.geoBlockAnalyzer = new GeoBlockAnalyzer(4, 7, "src/main/resources/geosSmall.csv");
-        final GeoResult result = this.geoBlockAnalyzer.getLargestGeoBlock();
-        assertEquals(4, result.getSize());
+  @Test
+  void getLargestGeoBlockTestOne() throws IOException {
+    this.geoBlockAnalyzer = new GeoBlockAnalyzer(4, 7, "src/main/resources/geosSmall.csv");
+    final GeoResult result = this.geoBlockAnalyzer.getLargestGeoBlock();
+    assertEquals(4, result.getSize());
+  }
+
+  @Test
+  void getLargestGeoBlockTestTwo() throws IOException {
+    this.geoBlockAnalyzer = new GeoBlockAnalyzer(7, 4, "src/main/resources/geosSmall.csv");
+    final GeoResult result = this.geoBlockAnalyzer.getLargestGeoBlock();
+    assertEquals(5, result.getSize());
+  }
+
+  @Test
+  void getLargestGeoBlockTestLarge() throws IOException {
+    this.geoBlockAnalyzer = new GeoBlockAnalyzer(10000, 10000, "src/main/resources/geosLarge.csv");
+    final GeoResult result = this.geoBlockAnalyzer.getLargestGeoBlock();
+    assertEquals(8, result.getSize());
+  }
+
+  @Test
+  void missingDataExceptionTest() throws IOException {
+    try {
+      this.geoBlockAnalyzer =
+          new GeoBlockAnalyzer(100, 300, "src/main/resources/geosWithMissingData.csv");
+    } catch (final IllegalArgumentException e) {
+      assertTrue(e.getMessage().contains("There is missing data in the csv file at line: 4"));
+    }
+  }
+
+  @Test
+  void invalidInputExceptionTest() throws IOException {
+    try {
+      this.geoBlockAnalyzer = new GeoBlockAnalyzer(0, 5, "src/main/resources/geosSmall.csv");
+    } catch (final IllegalArgumentException e) {
+      assertTrue(e.getMessage().contains("Input height or width is 0 or smaller"));
     }
 
-    @org.junit.jupiter.api.Test
-    void getLargestGeoBlockTestTwo() throws IOException {
-        this.geoBlockAnalyzer = new GeoBlockAnalyzer(7, 4, "src/main/resources/geosSmall.csv");
-        final GeoResult result = this.geoBlockAnalyzer.getLargestGeoBlock();
-        assertEquals(5, result.getSize());
+    try {
+      this.geoBlockAnalyzer = new GeoBlockAnalyzer(-1, -8, "src/main/resources/geosSmall.csv");
+    } catch (final IllegalArgumentException e) {
+      assertTrue(e.getMessage().contains("Input height or width is 0 or smaller"));
     }
-
-    @org.junit.jupiter.api.Test
-    void getLargestGeoBlockTestLarge() throws IOException {
-        this.geoBlockAnalyzer = new GeoBlockAnalyzer(10000, 10000, "src/main/resources/geosLarge.csv");
-        final GeoResult result = this.geoBlockAnalyzer.getLargestGeoBlock();
-        assertEquals(8, result.getSize());
-    }
-
-    @org.junit.jupiter.api.Test
-    void missingDataExceptionTest() throws IOException {
-        try {
-            this.geoBlockAnalyzer = new GeoBlockAnalyzer(100, 300, "src/main/resources/geosWithMissingData.csv");
-        }
-        catch(final IllegalArgumentException e){
-            assertTrue(e.getMessage().contains("There is missing data in the csv file at line: 4"));
-        }
-    }
-
-    @org.junit.jupiter.api.Test
-    void invalidInputExceptionTest() throws IOException {
-        try {
-            this.geoBlockAnalyzer = new GeoBlockAnalyzer(0, 5, "src/main/resources/geosSmall.csv");
-        }
-        catch(final IllegalArgumentException e){
-            assertTrue(e.getMessage().contains("Input height or width is 0 or smaller"));
-        }
-
-        try {
-            this.geoBlockAnalyzer = new GeoBlockAnalyzer(-1, -8, "src/main/resources/geosSmall.csv");
-        }
-        catch(final IllegalArgumentException e){
-            assertTrue(e.getMessage().contains("Input height or width is 0 or smaller"));
-        }
-    }
+  }
 }
