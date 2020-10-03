5259041128866789d3090c14fcbf96604a44477d
Eoin Reilly <eoindreilly@gmail.com>
2020-10-03 19:50:41 +0100

    Revert "Deleted App.java as it is not needed"
    This reverts commit 899c43c1363513d16e610b4d53e288e887f84dbc.

---
+++ src/main/java/analyzer/block/geo/main/App.java
@@ -0,0 +1,21 @@
+package analyzer.block.geo.main;
+
+import analyzer.block.geo.result.GeoResult;
+
+import java.io.IOException;
+
+public class App {
+
+    public static void main(final String[] args) throws IOException {
+        final GeoBlockAnalyzer geoBlockAnalyzer =
+                new GeoBlockAnalyzer(10000, 10000, "src/main/resources/geosLarge.csv");
+        final long start = System.currentTimeMillis();
+        final GeoResult result = geoBlockAnalyzer.getLargestGeoBlock();
+        final long end = System.currentTimeMillis();
+
+        System.out.println("Run time: " + (end - start) + " ms");
+        System.out.println(result.toString());
+
+        System.out.println(5 / 7);
+    }
+}
